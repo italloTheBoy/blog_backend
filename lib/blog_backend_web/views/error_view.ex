efe3607e188cdc645fcd6c0c3e4487f95b02dc10
itallo <itallomkspop@gmail.com>
2022-10-26 21:50:50 -0300

    replace changeset error view with a error view

--- lib/blog_backend_web/views/error_view.ex
+++ lib/blog_backend_web/views/error_view.ex
@@ -3,23 +3,20 @@ defmodule BlogBackendWeb.ErrorView do
 
   import BlogBackendWeb.ChangesetHelpers
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
-  def render("changeset.json", %{changeset: changeset, message: message}) do
+  def render("error.json", %{changeset: changeset, message: message}) do
     %{
       payload: put_changes(changeset),
       errors: put_errors(changeset, message)
     }
   end
 
-  def render("changeset.json", %{changeset: changeset}) do
+  def render("error.json", %{message: message}) do
+    %{
+      errors: put_errors(message)
+    }
+  end
+
+  def render("error.json", %{changeset: changeset}) do
     %{
       payload: put_changes(changeset),
       errors: put_errors(changeset)
