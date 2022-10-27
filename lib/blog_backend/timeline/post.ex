e08b886dd6627a173a6775f47b967a68b610e7a3
itallo <itallomkspop@gmail.com>
2022-10-26 21:04:27 -0300

    post validations

--- lib/blog_backend/timeline/post.ex
+++ lib/blog_backend/timeline/post.ex
@@ -6,8 +6,8 @@ defmodule BlogBackend.Timeline.Post do
   alias BlogBackend.Auth.User
 
   schema "posts" do
-    field :text, :string
     field :title, :string
+    field :body, :string
 
     belongs_to :user, User
 
@@ -15,13 +15,32 @@ defmodule BlogBackend.Timeline.Post do
   end
 
   @spec changeset(
-          %BlogBackend.Timeline.Post{},
+          %__MODULE__{},
           :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
         ) :: Ecto.Changeset.t()
   @doc false
   def changeset(post = %__MODULE__{}, attrs) do
     post
-    |> cast(attrs, [:title, :text])
-    |> validate_required([:title, :text])
+    |> cast(attrs, [:title, :body, :user_id])
+    |> validate_required([:title, :body], message: "Preencha este campo")
+    |> validate_title()
+    |> validate_body()
+    |> validate_user()
+  end
+
+  defp validate_title(%Ecto.Changeset{} = changeset) do
+    changeset
+    |> validate_length(:title, max: 50, message: "Título miuto longo")
+  end
+
+  defp validate_body(%Ecto.Changeset{} = changeset) do
+    changeset
+    |> validate_length(:body, max: 500, message: "Conteúdo miuto longo")
+  end
+
+  defp validate_user(changeset) do
+    changeset
+    |> validate_required([:user_id], message:  "Login necessário")
+    |> assoc_constraint(:user, message: "Login inválido")
   end
 end
