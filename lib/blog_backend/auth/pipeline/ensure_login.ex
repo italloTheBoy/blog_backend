a396a0f05384d08dc3f9545da47d903856128f0f
itallo <itallomkspop@gmail.com>
2022-10-26 21:04:59 -0300

    fix ensure_login olug

--- lib/blog_backend/auth/pipeline/ensure_login.ex
+++ lib/blog_backend/auth/pipeline/ensure_login.ex
@@ -7,7 +7,10 @@ defmodule BlogBackend.Auth.Pipeline.EnsureLogin do
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
   def call(conn, _params) do
     current_user = Guardian.Plug.current_resource(conn)
-    user_id = conn.path_params["id"]
+
+    user_id =
+      conn.path_params["user_id"]
+      || conn.path_params["id"]
 
     if current_user.id == String.to_integer(user_id) do
       conn
