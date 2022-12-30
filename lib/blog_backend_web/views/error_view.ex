c2d99d50b93f7a84274c6346f9c4c9f1cf4283b1
Itallo <itallomkspop@gmail.com>
2022-12-30 13:27:58 -0300

    test error_view

--- lib/blog_backend_web/views/error_view.ex
+++ lib/blog_backend_web/views/error_view.ex
@@ -25,17 +25,17 @@ defmodule BlogBackendWeb.ErrorView do
   end
 
   def template_unauthorized(template, _assigns),
-    do: %{errors: %{message: status_message_from_template(template)}}
+    do: %{errors: %{detail: status_message_from_template(template)}}
 
   def template_forbidden(template, _assigns),
-    do: %{errors: %{message: status_message_from_template(template)}}
+    do: %{errors: %{detail: status_message_from_template(template)}}
 
   def template_not_found(template, _assigns),
-    do: %{errors: %{message: status_message_from_template(template)}}
+    do: %{errors: %{detail: status_message_from_template(template)}}
 
   def template_unprocessable_entity(template, _assigns),
-    do: %{errors: %{message: status_message_from_template(template)}}
+    do: %{errors: %{detail: status_message_from_template(template)}}
 
   def template_internal_server_error(template, _assigns),
-    do: %{errors: %{message: status_message_from_template(template)}}
+    do: %{errors: %{detail: status_message_from_template(template)}}
 end
--- test/blog_backend_web/views/error_view_test.exs
+++ test/blog_backend_web/views/error_view_test.exs
@@ -1,12 +1,44 @@
 defmodule BlogBackendWeb.ErrorViewTest do
   use BlogBackendWeb.ConnCase, async: true
 
-  test "renders 404.json" do
-    assert render(BlogBackendWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  import BlogBackendWeb.ErrorView
+
+  alias BlogBackendWeb.ErrorView
+
+  @moduletag :error_view
+
+  @tag error_view: "401"
+  test "renders 401.json" do
+    assert render(ErrorView, "401.json") == %{
+             errors: %{detail: "Unauthorized"}
+           }
   end
 
+  @tag error_view: "403"
+  test "renders 403.json" do
+    assert render(ErrorView, "403.json") == %{
+             errors: %{detail: "Forbidden"}
+           }
+  end
+
+  @tag error_view: "404"
+  test "renders 404.json" do
+    assert render(ErrorView, "404.json") == %{
+             errors: %{detail: "Not Found"}
+           }
+  end
+
+  @tag error_view: "422"
+  test "renders 422.json" do
+    assert render(ErrorView, "422.json") == %{
+             errors: %{detail: "Unprocessable Entity"}
+           }
+  end
+
+  @tag error_view: "500"
   test "renders 500.json" do
-    assert render(BlogBackendWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+    assert render(ErrorView, "500.json") == %{
+             errors: %{detail: "Internal Server Error"}
+           }
   end
 end
